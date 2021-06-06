131447462a4a1e8f150810971f119a1cb60fd992
Rodolpho Alcântara <rodolphoalcantara@outlook.com>
2021-06-06 19:07:52 -0300

    validacao dos formularios da modal

--- adimo/js/app.js
+++ adimo/js/app.js
@@ -1,17 +1,21 @@
-$("nav a").click(function (e) {
+$("nav a").click(function navigate(e) {
   e.preventDefault();
   var id = $(this).attr("href");
-  var targetOffset = $(id).offset().top;
-  var menuHeight = $("nav").innerHeight();
+  if (id != "login.html") {
+    var targetOffset = $(id).offset().top;
+    var menuHeight = $("nav").innerHeight();
 
-  console.log(id);
+    console.log(id);
 
-  $("html, body").animate(
-    {
-      scrollTop: targetOffset - menuHeight,
-    },
-    500
-  );
+    $("html, body").animate(
+      {
+        scrollTop: targetOffset - menuHeight,
+      },
+      500
+    );
+  }else{
+    window.location.href = 'login.html';
+  }
 });
 
 $("#btnMais").click(function () {
@@ -24,13 +28,50 @@ $("#btnMenos").click(function () {
   $(".painel-aberto").addClass("painel").removeClass("painel-aberto");
   $("#btnMenos").addClass("hidden").removeClass("opened");
   $("#btnMais").addClass("opened").removeClass("hidden");
+
+  $("html, body").animate(
+    {
+      scrollTop: $("#catalogo").offset().top - $("nav").innerHeight(),
+    },
+    500
+  );
 });
 
 $("#agendar").click(function () {
-  var checado = $('#catalogo').find("input[name='checkboxImg']:checked").length > 0;
+  var checado =
+    $("#catalogo").find("input[name='checkboxImg']:checked").length > 0;
   if (!checado) {
     alert("Você deve selecionar pelo menos um imóvel!");
   } else {
-    alert("Obrigado por agendar uma visita");
+    $("#addUsuarioModal").modal("show");
+  }
+});
+
+$("#modalSalvar").click(function (e) {
+  e.preventDefault;
+
+  $("#addUsuarioModal input").each(function () {
+    if ($(this).val() === "") {
+      $(this).addClass("is-invalid");
+      if ($(this).hasClass("is-valid")) {
+        $(this).removeClass("is-valid");
+      }
+    } else {
+      $(this).addClass("is-valid");
+      if ($(this).hasClass("is-invalid")) {
+        $(this).removeClass("is-invalid");
+      }
+    }
+  });
+
+  var valid = true;
+  $("#addUsuarioModal input").each(function () {
+    if ($(this).hasClass("is-invalid")) valid = false;
+    return valid;
+  });
+
+  if (valid) {
+    alert("Obrigado por informar seus dados");
+    $("#addUsuarioModal").modal("hide");
   }
 });
